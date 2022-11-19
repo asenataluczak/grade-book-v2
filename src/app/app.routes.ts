4a3ef1c0500739341d54d0c87b58fc835eb44d9a
Secia <asenata.luczak@gmail.com>
2022-11-19 21:32:55 +0100

    introduced register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
 
-export const APP_ROUTES: Routes = [{ path: 'login', component: LoginComponent },]
+export const APP_ROUTES: Routes = [{ path: 'login', component: LoginComponent }, { path: 'register', component: RegisterComponent }]
