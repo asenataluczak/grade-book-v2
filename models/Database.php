5cbab74b2363553bd7b933f106e43c0b5fe9c77a
Secia <asenata.luczak@gmail.com>
2022-11-20 19:42:45 +0100

    create Database class

---
+++ models/Database.php
@@ -0,0 +1,56 @@
+<?php
+
+class Database
+{
+    protected $connection = null;
+    private $config = null;
+
+    public function __construct()
+    {
+        $this->config = parse_ini_file('../private/credentials.ini');
+        try {
+            $this->connection = mysqli_connect($this->config['db_host'], $this->config['db_user'], $this->config['db_password'], $this->config['db_name'], $this->config['db_port']);
+
+            if (mysqli_connect_errno()) {
+                throw new Exception("Could not connect to database.");
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    public function select($query = "", $params = [])
+    {
+        try {
+            $stmt = $this->executeStatement($query, $params);
+            $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+            $stmt->close();
+
+            return $result;
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+        return false;
+    }
+
+    private function executeStatement($query = "", $params = [])
+    {
+        try {
+            $stmt = $this->connection->prepare($query);
+
+            if ($stmt === false) {
+                throw new Exception("Unable to do prepared statement: " . $query);
+            }
+
+            if ($params) {
+                $stmt->bind_param($params[0], $params[1]);
+            }
+
+            $stmt->execute();
+
+            return $stmt;
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+}
