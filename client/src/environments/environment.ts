f04cb58e4c94cab2241e9e280f0f106150b4bbc9
Secia <asenata.luczak@gmail.com>
2022-11-24 20:42:24 +0100

    fixed be path

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  backendUrl: 'http://localhost:8041/BE-php-grade-book/index.php'
+  backendUrl: 'http://localhost:8041/grade-book-v2/api/index.php'
 };
 
