8331da74cf3cbfa251ccaedcd2ffbf0b69a97134
Secia <asenata.luczak@gmail.com>
2022-11-26 21:05:20 +0100

    improved index.php

--- api/index.php
+++ api/index.php
@@ -2,18 +2,21 @@
 require "./bootstrap.php";
 
 header("Access-Control-Allow-Origin: *");
- 
+
+$filename = basename(__FILE__);
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode( '/', $uri );
- 
-if ((isset($uri[3]) && $uri[3] != 'user') || !isset($uri[4])) {
+$path = substr($uri, stripos($uri, $filename) + strlen($filename));
+$pathSegments = array_values(array_filter(explode('/', $path), fn ($x) => strlen($x) > 0));
+$segmentsNumber = count($pathSegments);
+
+if ($segmentsNumber != 2 || $pathSegments[0] != 'user') {
     header("HTTP/1.1 404 Not Found");
     exit();
 }
- 
+
 require "./controllers/UserController.php";
- 
+
 $objFeedController = new UserController();
-$strMethodName = $uri[4] . 'Action';
+$strMethodName = $pathSegments[1] . 'Action';
 $objFeedController->{$strMethodName}();
-?>
