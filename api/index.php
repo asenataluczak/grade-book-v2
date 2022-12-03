82a625ea3c2ebd5ab81c28ad3b5843335611a069
Secia <asenata.luczak@gmail.com>
2022-12-03 20:45:46 +0100

    fixed cors error

--- api/index.php
+++ api/index.php
@@ -1,7 +1,15 @@
 <?php
 require "./bootstrap.php";
 
-header("Access-Control-Allow-Origin: *");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
 
 $filename = basename(__FILE__);
 
