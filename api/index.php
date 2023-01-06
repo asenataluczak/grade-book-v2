fb1a10e609b0d68baacc5b73964a53571d76acf9
asenataluczak <asenata.luczak@gmail.com>
2023-01-06 14:32:44 +0100

    refactored index

--- api/index.php
+++ api/index.php
@@ -1,16 +1,21 @@
 <?php
 require "./bootstrap.php";
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-header('Access-Control-Allow-Headers: token, Content-Type');
 
 $filename = basename(__FILE__);
-
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $path = substr($uri, stripos($uri, $filename) + strlen($filename));
 $pathSegments = array_values(array_filter(explode('/', $path), fn ($x) => strlen($x) > 0));
-$segmentsNumber = count($pathSegments);
 
 
 if ($pathSegments[0] == 'user') {
