813b94931feea82145a5b0dfde704949da2cc4f0
asenataluczak <asenata.luczak@gmail.com>
2023-01-07 18:44:52 +0100

    updated user grades

--- client/src/app/components/dashboard/grades-student/grades-student.component.ts
+++ client/src/app/components/dashboard/grades-student/grades-student.component.ts
@@ -1,19 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { Course } from 'src/app/interfaces/course.interface';
+import { GradeService } from 'src/app/services/grade.service';
+import { Grade } from 'src/app/interfaces/grade.interface';
+import { GradeDialogComponent } from '../dialogs/grade-dialog/grade-dialog.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-grades-student',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatDialogModule],
   templateUrl: './grades-student.component.html',
   styleUrls: ['./grades-student.component.scss']
 })
 export class GradesStudentComponent {
-  courses = [{name: 'Matematyka'}, {name: 'Przyroda'}];
+  courses: Array<Course>;
   userId = 2;
-  allGrades = [{grades: [{value: 2}, {value: 5}]}];
+  allGrades: any;
 
-  openViewGrade(grade: any, course: any) {
+  constructor(private gradeService: GradeService, public dialog: MatDialog) {
+    this.courses = this.gradeService.getCourses();
+    this.gradeService.getGradesPerUser(+localStorage.getItem('userId')!).subscribe(data => {
+      this.allGrades = data;
+    })
+  }
+
+  getGradesPerCourse(courseId: number) {
+    return this.allGrades?.filter((grade: any) => grade.courseId === courseId)
+  }
+
+  openViewGrade(grade: Grade, course: any) {
+    this.dialog.open(GradeDialogComponent, {
+      data: { grade, course },
+      width: '700px'
+    });
+
   }
 }
